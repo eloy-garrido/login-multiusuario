c093cf1cd8792f4a1581ac7555dd0d93b03c7e75
Eloy Garrido <eloegf@gmail.com>
2025-09-30 14:31:27 -0300

    fix ssr

--- frontend/src/main.server.ts
+++
@@ -1,8 +0,0 @@
-import 'zone.js/node';
-import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
-import { AppComponent } from './app/app';
-import { config } from './app/app.config.server';
-
-const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, config, context);
-
-export default bootstrap;
